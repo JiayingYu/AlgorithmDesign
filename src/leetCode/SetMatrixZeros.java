fc067299d58b65249e0073d4e0addab4322e9bd4
Jiaying Yu <jiaying.yu@nyu.edu>
2015-02-05 18:24:14 -0500

    fix bugs

---
+++ src/leetCode/SetMatrixZeros.java
@@ -0,0 +1,47 @@
+package leetCode;
+
+public class SetMatrixZeros {
+	public void setZeroes(int[][] matrix) {
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		boolean colZero = false;
+		boolean rowZero = false;
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+
+					if (i == 0) {
+						rowZero = true;
+					}
+
+					if (j == 0) {
+						colZero = true;
+					}
+				}
+			}
+		}
+
+		for (int i = 1; i < rows; i++) {
+			for (int j = 1; j < cols; j++) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		if (colZero) {
+			for (int i = 0; i < rows; i++) {
+				matrix[i][0] = 0;
+			}
+		}
+
+		if (rowZero) {
+			for (int j = 0; j < cols; j++) {
+				matrix[0][j] = 0;
+			}
+		}
+	}
+}
